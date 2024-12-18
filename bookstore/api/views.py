b7ce7e0ae6f4b32f98001edeb584cbdeaf4cfea9
Sergey Morgunov <Bo-Jack-Bo@yandex.ru>
2024-12-18 21:31:13 +0400

    Добавил атомарность покупок, исправил запросы к БД.

--- bookstore/api/views.py
+++ bookstore/api/views.py
@@ -6,33 +6,41 @@ from rest_framework.response import Response
 from books.models import Author, Book
 from .filters import BookFilter
 from .serializers import AuthorSerializer, BookSerializer
+from django.db import transaction
+from django.db.models import F
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
-    queryset = Author.objects.all()
+    queryset = Author.objects.all().prefetch_related('books')
     serializer_class = AuthorSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().select_related('author')
     serializer_class = BookSerializer
     filter_backends = (DjangoFilterBackend,)
     filterset_class = BookFilter
 
     @action(detail=True, methods=['post'], url_path='buy')
     def buy(self, request, pk=None):
-        book = self.get_object()
+        try:
+            with transaction.atomic():
+                updated_count = Book.objects.filter(
+                    id=pk, count__gt=0
+                ).update(count=F('count') - 1)
+                if updated_count == 0:
+                    return Response(
+                        {'detail': 'Книги нет на складе.'},
+                        status=status.HTTP_400_BAD_REQUEST
+                    )
 
-        if book.count <= 0:
             return Response(
-                {'detail': 'Книги нет на складе.'},
-                status=status.HTTP_400_BAD_REQUEST
+                {'message': 'Книга успешно куплена.'},
+                status=status.HTTP_200_OK
             )
 
-        book.count -= 1
-        book.save()
-
-        return Response(
-            {'message': 'Книга успешно куплена.'},
-            status=status.HTTP_200_OK
-        )
+        except Exception as e:
+            return Response(
+                {'detail': 'Ошибка при покупке книги: ' + str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
